52208372ae3d033c7b40056fe69e16ac54d3914d
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2017-01-10 23:16:58 -0600

    Material working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = '';
 }
