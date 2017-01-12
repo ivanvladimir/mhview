89e488236bfb03d60a17430ec60b157181624486
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2017-01-11 23:21:49 -0600

    Adds render service

--- src/app/render.service.ts
+++ src/app/render.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RenderService {
+  public md;
 
-  constructor() { }
+  constructor() { 
+    let Remarkable = require('remarkable');
+    this.md        = new Remarkable({
+      html: true,
+    breaks: false})
+  }
 
+  render (text: String) {
+        return this.md.render(text);
+  }
 }
